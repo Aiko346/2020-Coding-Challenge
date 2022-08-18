f429c83b1c495ed907d1c972c5f043a4acde24db
Aiko Gerra <aikogerra@aikos-air.myfiosgateway.com>
2022-08-17 20:06:07 -0400

    Create desired behavior for increment button.

--- server.py
+++ server.py
@@ -50,7 +50,8 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-
+            
+    scoreboard.sort(reverse=True, key=lambda item: item["score"])
     return jsonify(scoreboard=scoreboard)
 
 
